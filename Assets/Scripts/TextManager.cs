4cdcfe32d1439c8dc8532216d64e769497468588
Carl Christensen <carl.christensen98@gmail.com>
2023-10-30 11:25:56 +0100

    Initial commit

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+
+    private int coins = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = coins.ToString();
+    }
+
+    public void AddCoins()
+    {
+        coins++;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Skolan2D
+ 2D project för ITA23
